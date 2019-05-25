a78e17a87e6ea68d367181c49cad172ee03e8431
Andrea Lacava <thecave003@gmail.com>
2019-05-25 13:55:43 +0200

    voice activity

--- app/src/main/java/it/sapienza/mysecurefolder/VoiceActivity.java
+++ app/src/main/java/it/sapienza/mysecurefolder/VoiceActivity.java
@@ -1,7 +1,17 @@
 package it.sapienza.mysecurefolder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.microsoft.cognitive.speakerrecognition.SpeakerIdentificationRestClient;
+import com.microsoft.cognitive.speakerrecognition.SpeakerVerificationClient;
+import com.microsoft.cognitive.speakerrecognition.SpeakerVerificationRestClient;
+import com.microsoft.cognitive.speakerrecognition.contract.verification.Profile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.Properties;
 
 public class VoiceActivity extends AppCompatActivity {
 
@@ -9,5 +19,16 @@ public class VoiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voice);
+        Properties properties = new Properties();
+        InputStream inputStream =
+                Objects.requireNonNull(this.getClass().getClassLoader()).getResourceAsStream("email.properties");
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        SpeakerVerificationClient client = new SpeakerVerificationRestClient(properties.getProperty("apikey.voice.key1"));
+        //Profile profile = client.getProfile("");
+
     }
 }
