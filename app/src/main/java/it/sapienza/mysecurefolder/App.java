0eebefdea4b3fb921093344aa90afd9c80b7db95
CavalloAndrea <andreacavallossl@gmail.com>
2019-06-26 13:16:08 +0200

    Tanto non funziona!

--- app/src/main/java/it/sapienza/mysecurefolder/App.java
+++ app/src/main/java/it/sapienza/mysecurefolder/App.java
@@ -2,12 +2,28 @@ package it.sapienza.mysecurefolder;
 
 import android.app.Application;
 
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 
 public class App extends Application {
+
+    public static final String BASE_URL = "https://secure-folder.herokuapp.com/";
+
+
+
+
+    public static String getBaseUrl() {
+        return BASE_URL;
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
+
     }
 
 }
--- app/src/main/java/it/sapienza/mysecurefolder/InitActivity.java
+++ app/src/main/java/it/sapienza/mysecurefolder/InitActivity.java
@@ -2,12 +2,33 @@ package it.sapienza.mysecurefolder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class InitActivity extends AppCompatActivity {
+    private final static String TAG = InitActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
+
+        String url = App.getBaseUrl() + "/list-group";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = new OkHttpClient().newCall(request).execute()) {
+            Log.d(TAG, "Risposta: ");
+            response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
