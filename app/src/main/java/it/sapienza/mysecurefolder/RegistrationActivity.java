9cfdc9a837a18dd23e3d27fa032e9670d3d7afd2
Andrea Lacava <thecave003@gmail.com>
2019-06-27 17:23:31 +0200

    voice activity

--- app/src/main/java/it/sapienza/mysecurefolder/RegistrationActivity.java
+++ app/src/main/java/it/sapienza/mysecurefolder/RegistrationActivity.java
@@ -1,80 +1,52 @@
 package it.sapienza.mysecurefolder;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
-import android.media.MediaPlayer;
-import android.media.MediaRecorder;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
-import android.widget.EditText;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import org.json.JSONObject;
-
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-import java.util.Objects;
-import java.util.UUID;
 
-import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-
-import static it.sapienza.mysecurefolder.App.BASE_URL;
-import static okhttp3.internal.http.HttpDate.format;
+import cafe.adriel.androidaudiorecorder.AndroidAudioRecorder;
+import cafe.adriel.androidaudiorecorder.model.AudioChannel;
+import cafe.adriel.androidaudiorecorder.model.AudioSampleRate;
+import cafe.adriel.androidaudiorecorder.model.AudioSource;
 
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    private static final String TAG = RegistrationActivity.class.getSimpleName();
     String currentImagePath = null;
     private static final int IMAGE_REQUEST = 1;
-    private static final int MY_CAMERA_PERMISSION_CODE = 100;
-    final int MY_VOICE_PERMISSION_CODE = 1000;
+    private static final int MY_CAMERA_PERMISSION_CODE = 128;
+    private static final int RECORD_REQUEST = 2;
+
     Button fotoButton;
     Button saveButton;
     ImageView imageView;
     Button btnStartRecord, btnStopRecord;
-    String pathSave = "";
-    MediaRecorder mediaRecorder;
-    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-
-        /*if (!checkPermissionFromDevice()) {
-            requestPermission();
-        }*/
 
         btnStartRecord = findViewById(R.id.btnStartRecord);
         btnStopRecord = findViewById(R.id.btnStopRecord);
@@ -83,9 +55,12 @@ public class RegistrationActivity extends AppCompatActivity {
         btnStartRecord = findViewById(R.id.btnStartRecord);
         btnStopRecord = findViewById(R.id.btnStopRecord);
         saveButton = findViewById(R.id.bottoneSalva);
+
+
         fotoButton.setOnClickListener(v -> {
-            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                    checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO}, MY_CAMERA_PERMISSION_CODE);
             } else {
                 Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (cameraIntent.resolveActivity(getPackageManager()) != null) {
@@ -105,38 +80,31 @@ public class RegistrationActivity extends AppCompatActivity {
                 }
             }
         });
-        /*
-        btnStartRecord.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (checkPermissionFromDevice()) {
 
 
-                    pathSave = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + UUID.randomUUID().toString() + "_audio_record.wav";
-                    setupMediaRecorder();
-                    try {
-                        mediaRecorder.prepare();
-                        mediaRecorder.start();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+        btnStartRecord.setOnClickListener(
+                view -> {
+                    String filePath = getExternalFilesDir(Environment.DIRECTORY_MUSIC).getPath() + "/recorded_audio.wav";
+                    Log.d(TAG, filePath);
+                    int color = getResources().getColor(R.color.colorPrimaryDark);
 
+                    AndroidAudioRecorder.with(this)
+                            .setFilePath(filePath)
+                            .setColor(color)
+                            .setRequestCode(RECORD_REQUEST)
+                            .setSource(AudioSource.MIC)
+                            .setChannel(AudioChannel.MONO)
+                            .setSampleRate(AudioSampleRate.HZ_16000)
+                            .setAutoStart(true)
+                            .setKeepDisplayOn(true)
+                            .record();
+                });
 
-                    Toast.makeText(RegistrationActivity.this, "Recording...", Toast.LENGTH_SHORT).show();
-                } else {
-                    requestPermission();
-                }
-            }
+        btnStopRecord.setOnClickListener(view -> {
+            // btnStartRecord.setEnabled(true);
+            // btnStopRecord.setEnabled(false);
         });
-
-        btnStopRecord.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mediaRecorder.stop()
-                btnStartRecord.setEnabled(true);
-                btnStopRecord.setEnabled(false);
-            }
-        });
+/*
         saveButton.setOnClickListener(v -> new Thread(() -> {
             EditText name = findViewById(R.id.name);
             OkHttpClient client = new OkHttpClient();
@@ -157,35 +125,40 @@ public class RegistrationActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }).start());*/
-
-
     }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        /*Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_VIEW);
-        startActivity(intent);
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case RECORD_REQUEST: {
+                if (resultCode == RESULT_OK) {
+                    Log.d(TAG, "onActivityResult:  Great! User has recorded and saved the audio file");
+                } else if (resultCode == RESULT_CANCELED) {
+                    Log.d(TAG, "onActivityResult:  Oops! User has canceled the recording");
+                }
+                break;
+            }
 
+            case IMAGE_REQUEST: {
+                Matrix matrix = new Matrix();
+                matrix.postRotate(270);
+                Bitmap bitmap = BitmapFactory.decodeFile(currentImagePath);
+                Bitmap rotate = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+                imageView.setImageBitmap(rotate);
+                break;
+            }
 
-        Bitmap bitmap = BitmapFactory.decodeFile(getIntent().getStringExtra("image_path"));
-        imageView.setImageBitmap(bitmap);
-         Bitmap bitmap = BitmapFactory.decodeFile(getIntent().getStringExtra("image_path"));
-        */
-        Matrix matrix = new Matrix();
-        matrix.postRotate(270);
-        Bitmap bitmap = BitmapFactory.decodeFile(currentImagePath);
-        Bitmap rotate = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-        imageView.setImageBitmap(rotate);
+            default:
+                Log.e(TAG, "onActivityResult: boh");
+        }
     }
-
 
     private File getImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ITALIAN).format(new Date());
         String imageName = "jpeg_" + timeStamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-
         File imageFile = File.createTempFile(imageName, ".jpg", storageDir);
         currentImagePath = imageFile.getAbsolutePath();
         return imageFile;
@@ -204,31 +177,4 @@ public class RegistrationActivity extends AppCompatActivity {
             }
         }
     }
-/*
-    private void setupMediaRecorder() {
-        mediaRecorder = new MediaRecorder();
-        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
-        mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.DEFAULT);
-        mediaRecorder.setOutputFile(pathSave);
-    }
-
-    private void requestPermission() {
-        requestPermission(RegistrationActivity.this, new String[]{
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.RECORD_AUDIO
-        }, MY_VOICE_PERMISSION_CODE);
-    }
-
-    private boolean checkPermissionFromDevice() {
-        int write_external_storage_result = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        int record_audio_result = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
-        return write_external_storage_result == PackageManager.PERMISSION_GRANTED &&
-                record_audio_result == PackageManager.PERMISSION_GRANTED;
-    }
-*/
-
 }
-
-
-
