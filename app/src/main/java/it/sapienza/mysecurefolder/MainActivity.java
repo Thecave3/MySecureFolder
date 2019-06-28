472e0ebdf36c09a765f5d701ec299007cac7f1f3
CavalloAndrea <andreacavallossl@gmail.com>
2019-06-28 10:02:01 +0200

    Aggiunto bottone initActivity

--- app/src/main/java/it/sapienza/mysecurefolder/MainActivity.java
+++ app/src/main/java/it/sapienza/mysecurefolder/MainActivity.java
@@ -2,13 +2,37 @@ package it.sapienza.mysecurefolder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static String TAG = InitActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String url = App.getBaseUrl() + "/list-group";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        OkHttpClient client = new OkHttpClient();
+        new Thread(() -> {
+            try (Response response = client.newCall(request).execute()) {
+                Log.d(TAG, "Risposta: " + response.body().string());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
