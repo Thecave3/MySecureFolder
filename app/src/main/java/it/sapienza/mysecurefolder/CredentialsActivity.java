d5b86aff6636e436fcd939501357c563ea00823f
giorgia95 <giorgia.fumelli@gmail.com>
2019-09-11 16:28:56 +0200

    new icon

--- app/src/main/java/it/sapienza/mysecurefolder/CredentialsActivity.java
+++ app/src/main/java/it/sapienza/mysecurefolder/CredentialsActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import org.json.JSONException;
